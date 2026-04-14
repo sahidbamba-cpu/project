6d158d8d7ee9ab754ac204bd97a9928ea6b188cd
sahidbamba-cpu <sahidbamba@gmail.com>
2026-04-14 00:08:00 -0400

    Create RunnerEdit.java

---
+++ RunnerEdit.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane;
+
+public class RunnerEdit {
+    public static void main(String[] args) {
+        String input;
+        int questions;
+        int missed;
+
+        input = JOptionPane.showInputDialog("How many questions are on the exam?");
+        questions = Integer.parseInt(input);
+
+        input = JOptionPane.showInputDialog("How many questions did the student miss?");
+        missed = Integer.parseInt(input);
+
+        Exam exam = new Exam(questions, missed);
+
+        String message = "Each question counts " + exam.getPointsEach();
+        message += " points.\nThe exam score is " + exam.getScore();
+        message += "\nThe exam grade is " + exam.getGrade();
+
+        JOptionPane.showMessageDialog(null, message);
+
+        System.exit(0);
+    }
+}
